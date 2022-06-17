79fb2778242cc68ceca79c4ac5b3331993aa10a7
Lucas Meyer <lucas.meyer@microsoft.com>
2022-06-17 11:20:04 -0700

    Rendered on Fri Jun 17 11:20:04 PDT 2022

--- post-articles.py
+++ post-articles.py
@@ -4,6 +4,7 @@
 ###    - Determines whether to post them to LinkedIn and/or Twitter
 ###    - Posts them to LinkedIn/Twitter
 
+from pydoc import plain
 import sys
 import os
 from bs4 import BeautifulSoup
@@ -13,10 +14,14 @@ import re
 def markdown_to_text(markdown_string):
     """ Converts a markdown string to plaintext """
 
+    # Remove markdown code blocks
     markdown_string = re.sub(r"```.*?```", '', markdown_string, 0, re.DOTALL)
+
+    # Remove YAML
     markdown_string = re.sub(r"---.*?---", '', markdown_string, 0, re.DOTALL)
     
-    markdown_string = '\n'.join([i for i in markdown_string.splitlines() if not i.startswith("#")])
+    # Remove headings
+    # markdown_string = '\n'.join([i for i in markdown_string.splitlines() if not i.startswith("#")])
 
     # md -> html -> text since BeautifulSoup can extract text cleanly
     html = markdown(markdown_string)
@@ -24,7 +29,17 @@ def markdown_to_text(markdown_string):
     # remove code snippets
     html = re.sub(r'<pre>(.*?)</pre>', ' ', html)
     html = re.sub(r'<code>(.*?)</code >', ' ', html)
+
+    # Remove headings
+    html = re.sub(r'<h1>(.*?)</h1>', ' ', html)
+    html = re.sub(r'<h2>(.*?)</h2>', ' ', html)
+    html = re.sub(r'<h3>(.*?)</h3>', ' ', html)
+    html = re.sub(r'<h4>(.*?)</h4>', ' ', html)
+    html = re.sub(r'<h5>(.*?)</h5>', ' ', html)
+
+    # recreate lists
     html = re.sub(r'<li>(.*?)</li>', r'- \g<1>', html)
+    
     # Remove Quarto markdown options
     html = re.sub(r'{(.*?)}', ' ', html)
 
@@ -34,20 +49,35 @@ def markdown_to_text(markdown_string):
 
     return text
 
+
+def generate_plaintext_for_file(filepath):
+    print(f"\n======> {filepath}\n")
+    with open(filepath) as f:
+        plaintext = markdown_to_text(f.read())
+        print(f"Length: {len(plaintext)} characters\n\n")
+        plaintext = re.sub(r"\n", '\n\n', plaintext, 0, re.DOTALL)
+        plaintext = re.sub(r"\n\n\n", '\n\n', plaintext, 0, re.DOTALL)
+        plaintext = re.sub(r"\n\n-", '\n-', plaintext, 0, re.DOTALL)
+        plaintext = re.sub(r"\n\n\n", '\n\n', plaintext, 0, re.DOTALL)
+        print(plaintext)
+    print(f"\n======\n")
+
+
+def generate_plaintext_for_directory(di):
+    print(f"Files and directories for {di}")
+    for root, dirs, files in os.walk(di):
+        for filename in files:
+            if filename.endswith("qmd") or filename.endswith("md"):
+                filepath = os.path.join(root, filename)
+                generate_plaintext_for_file(filepath)
+
 def main():
     post_directories = ["posts"]
 
     for di in post_directories:
-        print(f"Files and directories for {di}")
-        for root, dirs, files in os.walk(di):
-            for filename in files:
-                if filename.endswith("qmd") or filename.endswith("md"):
-                    filepath = os.path.join(root, filename)
-                    print(filepath)
-                    with open(filepath) as f:
-                        print(markdown_to_text(f.read()))
-
+        generate_plaintext_for_directory(di)
     return 0
+
 
 
 if __name__ == "__main__":
