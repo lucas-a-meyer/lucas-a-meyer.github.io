e3b7261245c8ced1bc73de246c53d2f310c3e23e
Lucas A. Meyer <lucas@meyerperin.com>
2022-06-22 15:38:20 -0700

    Rendered on Wed Jun 22 15:38:20 PDT 2022

--- autorender.sh
+++ autorender.sh
@@ -1,5 +1,4 @@
 cd /home/lucasmeyer/personal/blog/
-touch `date +"%Y-%m-%dT%H-%M-%S.log"`
 git pull
 source /home/lucasmeyer/personal/blog/.venv/bin/activate
 /usr/bin/python3 -m pip install -r /home/lucasmeyer/personal/blog/requirements.txt
