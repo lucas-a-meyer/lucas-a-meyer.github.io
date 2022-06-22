92b42e03e30f03fe04b9e11b443112b7bb57a682
root <root@Poseidon.localdomain>
2022-06-22 15:12:19 -0700

    Rendered on Wed Jun 22 15:12:19 PDT 2022

--- autorender.sh
+++ autorender.sh
@@ -1,5 +1,7 @@
+cd /home/lucasmeyer/personal/blog/
+touch `date +"%Y-%m-%d.log"`
 git pull
 source /home/lucasmeyer/personal/blog/.venv/bin/activate
 /usr/bin/python3 -m pip install -r /home/lucasmeyer/personal/blog/requirements.txt
-quarto render
+/usr/local/bin/quarto render
 /usr/bin/python3 /home/lucasmeyer/personal/twilio/rendered.py
