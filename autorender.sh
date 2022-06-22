74d71c8a86b86d9c6c53c9d4d3f2175d2465301f
Lucas Meyer <lucas.meyer@microsoft.com>
2022-06-22 14:43:06 -0700

    Rendered on Wed Jun 22 14:43:06 PDT 2022

--- autorender.sh
+++ autorender.sh
@@ -1,4 +1,6 @@
 git pull
+source /home/lucasmeyer/personal/blog/.venv/bin/activate
 /usr/bin/python3 -m pip install -r /home/lucasmeyer/personal/blog/requirements.txt
 quarto render
 /usr/bin/python3 /home/lucasmeyer/personal/twilio/rendered.py
+deactivate
