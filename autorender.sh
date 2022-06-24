c51a40a4524edbde3e7e55a4d893e6441851b6c4
Lucas Meyer <lucas.meyer@microsoft.com>
2022-06-24 14:40:40 -0700

    Work done by Fri Jun 24 14:40:40 PDT 2022

--- autorender.sh
+++ autorender.sh
@@ -3,5 +3,5 @@ git stash --include-untracked
 git pull
 /home/lucasmeyer/personal/blog/.venv/bin/activate
 /usr/bin/python3 -m pip install -r /home/lucasmeyer/personal/blog/requirements.txt
-/usr/local/bin/quarto render --quiet
+/usr/local/bin/quarto render
 /usr/bin/python3 /home/lucasmeyer/personal/blog/text_blog_rendered.py
