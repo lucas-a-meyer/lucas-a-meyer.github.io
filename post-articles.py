3f39238626303e75fab940abd5979e917fd42e1b
Lucas Meyer <lucas.meyer@microsoft.com>
2022-06-29 10:12:18 -0700

    Work done by Wed Jun 29 10:12:18 PDT 2022

--- post-articles.py
+++ post-articles.py
@@ -334,7 +334,7 @@ def process_file(filepath):
 
     # Check that we have Microsoft Clarity installed in the page
     if not front_matter_dict.get("include-in-header"):
-        front_matter_dict["include_in_header"] = "_msft-clarity.html"
+        front_matter_dict["include-in-header"] = "_msft-clarity.html"
 
     update_front_matter(filepath, front_matter_dict)
 
