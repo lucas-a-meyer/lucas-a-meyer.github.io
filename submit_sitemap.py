4bc7da8047beb1da2c6da8341735fad92e5ae676
Lucas Meyer <lucas.meyer@microsoft.com>
2022-07-14 20:20:01 -0700

    Work done by Thu Jul 14 20:20:01 PDT 2022

--- submit_sitemap.py
+++ submit_sitemap.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import json
 
 wait = 10
 url = "https://www.meyerperin.com/sitemap.xml"
@@ -7,3 +8,9 @@ print(f"Waiting {wait} seconds to submit sitemap...")
 time.sleep(wait)
 requests.get("https://www.google.com/ping?sitemap={url}")
 print(f"Sitemap {url} submitted to Google")
+
+headers = {"content-type": "application/json", "charset": "utf-8"}
+indexnow_url = "https://www.bing.com/indexnow"
+with open ("docs/sitemap.json") as fj:
+    indexnow_json = json.load(fj)
+requests.post(indexnow_url, indexnow_json, headers=headers)
