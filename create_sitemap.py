3f261fc88c8724b333c068192278f2a71329d682
Lucas Meyer <lucas.meyer@microsoft.com>
2022-07-14 19:42:34 -0700

    Work done by Thu Jul 14 19:42:34 PDT 2022

--- create_sitemap.py
+++ create_sitemap.py
@@ -1,9 +1,24 @@
 import os
+from re import L
+import pandas as pd
+import datetime as dt
+import requests
 
 posts_dir = "docs/posts"
-site_root = "https://www.meyerperin.com/posts/"
+
+site_pages = []
+site_root = "https://www.meyerperin.com/posts"
 for root, dirs, files in os.walk(posts_dir):
     for name in files:
+        d = {}
         target = os.path.join(root, name)
-        target = f"{site_root}{target[len(posts_dir):]}"
-        print(target)
+        last_updated_timestamp = os.path.getmtime (target)
+        last_updated_time = dt.datetime.utcfromtimestamp(last_updated_timestamp)
+        d["lastmod"] = last_updated_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        url_target = f"{site_root}{target[len(posts_dir):]}"
+        d["loc"] = url_target
+        site_pages.append(d)
+
+df_sites = pd.DataFrame(site_pages)
+df_sites.to_xml("docs/sitemap.xml", index=False, row_name="url", root_name="urlset", namespaces={"": "http://www.sitemaps.org/schemas/sitemap/0.9"})
+# requests.get
