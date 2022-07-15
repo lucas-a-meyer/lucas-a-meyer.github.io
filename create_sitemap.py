58ac85ce8f281210261719f6b3b93ca051ddee0e
Lucas Meyer <lucas.meyer@microsoft.com>
2022-07-14 17:32:58 -0700

    Rendered on Thu Jul 14 17:32:58 PDT 2022

--- create_sitemap.py
+++ create_sitemap.py
@@ -1,8 +1,9 @@
 import os
 
 posts_dir = "docs/posts"
+site_root = "https://www.meyerperin.com/posts/"
 for root, dirs, files in os.walk(posts_dir):
     for name in files:
         target = os.path.join(root, name)
-        target = target[len(posts_dir):]
+        target = f"{site_root}{target[len(posts_dir):]}"
         print(target)
