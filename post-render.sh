392533487fd4bcd77554c483b020dae8b0bca8ba
Lucas Meyer <lucas.meyer@microsoft.com>
2022-07-25 10:17:13 -0700

    Rendered on Mon Jul 25 10:17:13 PDT 2022

--- post-render.sh
+++ post-render.sh
@@ -3,7 +3,7 @@ if [ -z $QUARTO_PROJECT_RENDER_ALL+x ]
 then
     echo "Not pushing to git because this is not a full render"
 else
-    python3.10 post-articles.py
+    python3.10 process-articles.py
     python3.10 create_sitemap.py
     git add .; git commit -m "Rendered on `date`"; git push   
     python3.10 submit_sitemap.py
