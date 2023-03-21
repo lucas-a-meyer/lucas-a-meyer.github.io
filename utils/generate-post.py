03e5d100df30808244863f23d8d6f74f5182e72c
Lucas Meyer <lucas.meyer@microsoft.com>
2023-03-21 09:42:35 -0700

    New rendering

--- utils/generate-post.py
+++ utils/generate-post.py
@@ -9,15 +9,17 @@ openai.api_base = "https://eastus.api.cognitive.microsoft.com/"
 openai.api_version = "2022-12-01"
 openai.api_key = os.getenv("OPEN_AI_API_KEY")
 
-def generate_post(subject, assistant="a tool that helps influencers improve their reach"):
+def generate_post(subject, start="", end="", assistant="a tool that helps influencers improve their reach"):
     response = openai.Completion.create(
         engine="gpt-35-turbo",
         prompt=f"""
         <|im_start|>system\nAssistant is {assistant}<|im_end|>\n
-        <|im_start|>user\nGenerate a blog post about {subject}<|im_end|>\n
-        <|im_start|>user\nUse professional language, this will be posted to LinkedIn<|im_end|>\n
-        <|im_start|>user\nWrite in the style of Michael Lewis and Hemingway<|im_end|>\n
-        <|im_start|>user\nYour output should be ready to paste in a blog post<|im_end|>\n
+        <|im_start|>user\nGenerate an essay about {subject}<|im_end|>\n
+        <|im_start|>user\nUse professional language<|im_end|>\n
+        <|im_start|>user\nWrite in the style of Michael Lewis Moneyball<|im_end|>\n
+        <|im_start|>user\nYour essay should start with "{start}"<|im_end|>\n
+        <|im_start|>user\nYour essay should end with "{end}"<|im_end|>\n
+        <|im_start|>user\nDo not start paragraphs with "In conclusion"<|im_end|>\n
         <|im_start|>assistant\n
         """,
         temperature=0.5,
@@ -27,9 +29,14 @@ def generate_post(subject, assistant="a tool that helps influencers improve thei
 
     return response['choices'][0]['text']        
 
+assistant = "a helpful editor that can help you write a blog post"
+
 about = "how chesterton's fence can help a person who just joined a team to be more efficient"
+
 about = "Dan Gilbert's experiment where people decided that it was better to shock themselves than to be alone with their thoughts"
 
+about = "The importance of programmers and analysts learning what and how to delegate tasks to large language models"
+start = "In my career, I have worked as an individual contributor and as a manager. With large language models, "
+end = "If you are technically oriented and used to do everything yourself, you may find that learning to cede some control to LLMs is going to make you a lot more productive. "
 
-assistant = "the editor of a major newspaper, who is also the CEO of a tech company"
-print(generate_post(subject=about, assistant=assistant))
+print(generate_post(subject=about, start=start, end=end, assistant=assistant))
