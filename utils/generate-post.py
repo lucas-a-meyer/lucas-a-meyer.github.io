ac2a603c7cda561bc6f14c15c5158fd1ba25e6db
Lucas Meyer <lucas.meyer@microsoft.com>
2023-03-20 17:57:55 -0700

    New rendering

--- utils/generate-post.py
+++ utils/generate-post.py
@@ -9,13 +9,15 @@ openai.api_base = "https://eastus.api.cognitive.microsoft.com/"
 openai.api_version = "2022-12-01"
 openai.api_key = os.getenv("OPEN_AI_API_KEY")
 
-def generate_post(subject):
+def generate_post(subject, assistant="a tool that helps influencers improve their reach"):
     response = openai.Completion.create(
         engine="gpt-35-turbo",
         prompt=f"""
-        <|im_start|>system\nAssistant is a tool that helps influencers improve their reach<|im_end|>\n
-        <|im_start|>user\nGenerate a four paragraph blog post about {subject}<|im_end|>\n
-        <|im_start|>user\nTry to be a little funny and hopeful<|im_end|>\n
+        <|im_start|>system\nAssistant is {assistant}<|im_end|>\n
+        <|im_start|>user\nGenerate a blog post about {subject}<|im_end|>\n
+        <|im_start|>user\nUse professional language, this will be posted to LinkedIn<|im_end|>\n
+        <|im_start|>user\nWrite in the style of Michael Lewis and Hemingway<|im_end|>\n
+        <|im_start|>user\nYour output should be ready to paste in a blog post<|im_end|>\n
         <|im_start|>assistant\n
         """,
         temperature=0.5,
@@ -25,4 +27,9 @@ def generate_post(subject):
 
     return response['choices'][0]['text']        
 
-print(generate_post("I'm feeling overwhelmed about how many new AI launches are happening, but I think that AI will not replace us, people who use AI will, and therefore I'm learning as much as I can"))
+about = "how chesterton's fence can help a person who just joined a team to be more efficient"
+about = "Dan Gilbert's experiment where people decided that it was better to shock themselves than to be alone with their thoughts"
+
+
+assistant = "the editor of a major newspaper, who is also the CEO of a tech company"
+print(generate_post(subject=about, assistant=assistant))
