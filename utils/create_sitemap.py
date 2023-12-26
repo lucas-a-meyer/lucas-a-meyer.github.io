c3d21fdf9613afe7ef8c2e23a53fd6fe6e40e8df
Lucas Meyer <lucas.meyer@microsoft.com>
2023-12-26 11:12:02 -0800

    New rendering as of 2023-12-26 11:09:08

--- utils/create_sitemap.py
+++ utils/create_sitemap.py
@@ -4,23 +4,27 @@ import pandas as pd
 import datetime as dt
 import json
 
-posts_dir = "docs/posts"
+posts_dir = ["_site/posts", "_site/articles"]
 
 site_pages = []
 url_list = []
-site_root = "https://www.meyerperin.com/posts"
-for root, dirs, files in os.walk(posts_dir):
-    for name in files:
-        if name.endswith(".html"):
-            d = {}
-            target = os.path.join(root, name)
-            last_updated_timestamp = os.path.getmtime (target)
-            last_updated_time = dt.datetime.utcfromtimestamp(last_updated_timestamp)
-            d["lastmod"] = last_updated_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-            url_target = f"{site_root}{target[len(posts_dir):]}"
-            d["loc"] = url_target
-            site_pages.append(d)
-            url_list.append(url_target)
+site_root = "https://www.meyerperin.com"
+
+for dir in posts_dir:
+    # assign folder with dir excluding docs/
+    folder = dir[5:]
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            if name.endswith(".html"):
+                d = {}
+                target = os.path.join(root, name)
+                last_updated_timestamp = os.path.getmtime (target)
+                last_updated_time = dt.datetime.utcfromtimestamp(last_updated_timestamp)
+                d["lastmod"] = last_updated_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+                url_target = f"{site_root}{folder}/{target[len(dir)+1:]}"
+                d["loc"] = url_target
+                site_pages.append(d)
+                url_list.append(url_target)
 
 df_sites = pd.DataFrame(site_pages)
 df_sites.to_xml("docs/sitemap.xml", index=False, row_name="url", root_name="urlset", namespaces={"": "http://www.sitemaps.org/schemas/sitemap/0.9"})
