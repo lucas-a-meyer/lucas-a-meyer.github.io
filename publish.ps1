874fd92643db436bb6275d3b0765791d6a728594
Lucas Meyer <lucas.meyer@microsoft.com>
2024-01-02 15:49:12 -0800

    New rendering as of 2024-01-02 15:49:12

--- publish.ps1
+++ publish.ps1
@@ -2,15 +2,15 @@
 $now = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 Write-Output "Publish started at $now"
 
-# Build the site
-quarto publish gh-pages
-# python utils/process-articles.py
-python utils/create_sitemap.py
-
 git add .
 git commit -m "New rendering as of $now"
 git push   
 
+# Build the site
+quarto publish gh-pages --no-prompt
+
+# python utils/process-articles.py
+python utils/create_sitemap.py
 python utils/submit_sitemap.py
 
 $now = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
